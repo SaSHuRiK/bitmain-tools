34f00a4f04d3676a827c0d98502a84726c3600aa
psycodad <source@netcraft.ch>
2018-06-21 10:32:28 +0200

    attempt at fixing for l3++ json output

--- scripts/l3plus_autotune.py
+++ scripts/l3plus_autotune.py
@@ -29,7 +29,7 @@
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 # --------------------------------------------------------------------------
 # l3plus_autotune.py: automatically tune undervolting on BM L3+/L3++
-# $Id: l3plus_autotune.py,v 1.57 2018-06-17 14:13:05 obiwan Exp $
+# $Id: l3plus_autotune.py,v 1.58 2018-06-20 16:49:28 obiwan Exp $
 # --------------------------------------------------------------------------
 
 # encode/decode trick with perl courtesy of:
@@ -101,14 +101,14 @@ def get_minerstats(ip, port=API_PORT):
     sys.exit(1)
   try:
     if json_resp.find('Blissz v1.02"}') > -1:
-      json_resp = json_resp.replace('Blissz v1.02"}', 'Blissz v1.02"},')
+      json_resp = json_resp.replace('Blissz v1.02"}', 'Blissz v1.02"},').strip()
       #print "Blissz fw detected, fixing json output accordingly"
     elif json_resp.find('Antminer L3++') > -1:
-      json_resp = json_resp.replace('Antminer L3++"}', 'Antminer L3++"},')
+      json_resp = json_resp.replace('Antminer L3++"}', 'Antminer L3++"},').strip()
       #print "L3++ fw detected, fixing json output accordingly"
     else:
       json_resp = json_resp.replace('\x00','')
-      json_resp = json_resp.replace("L3+\"}", "L3+\"},")
+      json_resp = json_resp.replace("L3+\"}", "L3+\"},").strip()
       #print "Bitmain fw detected, fixing json output accordingly"
     resp = json.loads(json_resp)
   except ValueError, e:
